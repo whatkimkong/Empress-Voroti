b914fb9728bb02547fe56fb8d3425d7f07079044
Jorge <jorgeberrizbeitia@gmail.com>
2021-07-01 15:12:44 +0200

    initial code

---
+++ script.js
@@ -0,0 +1,20 @@
+// INITIAL CANVAS STRUCTURE
+const canvas = document.querySelector("#my-canvas");
+canvas.style.backgroundColor = "lightGray"
+
+// stablish context. Enables paintbrush.
+const ctx = canvas.getContext("2d");
+
+// GLOBAL VARIABLES
+
+
+// GLOBAL FUNCTIONS
+
+
+// GAME LOOP FUNCTION
+
+
+// EVENT LISTENERS
+
+
+// GAME START
